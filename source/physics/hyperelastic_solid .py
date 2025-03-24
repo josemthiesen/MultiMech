65d44e0685b7973d19e2334faf5a527ff26b294b
Matheus-Janczkowski <matheusj2009@hotmail.com>
2025-03-24 11:24:33 -0300

    adds the physics directory to store variational problems

---
+++ source/physics/hyperelastic_solid .py
@@ -0,0 +1,11 @@
+# Routine to store the variational form and other accessories for a hy-
+# perelastic solid mechanics problem
+
+from dolfin import *
+
+# Defines a function to model a hyperelastic problem with a displacement
+# field only
+
+def hyperelasticity_displacementBased():
+
+    pass
