7d2b218323fe71ab148b795ba2d0a95552d648dc
Matheus-Janczkowski <matheusj2009@hotmail.com>
2025-05-06 21:53:21 -0300

    adds the automated tests for different kinds of simulations

---
+++ tests/various_physics_tests.py
@@ -0,0 +1,63 @@
+# Routine to store some tests
+
+import unittest
+
+# Defines a class to test some ready-to-run simulations
+
+class TestANNTools(unittest.TestCase):
+
+    def setUp(self):
+
+        pass
+
+    # Defines a function to test the Cauchy-continuum Neo-Hookean model
+    
+    def test_cauchyNeoHookean(self):
+
+        print("\n#####################################################"+
+        "###################\n#                          Cauchy Neo-Ho"+
+        "okean                          #\n###########################"+
+        "#############################################\n")
+
+        import tests.hyperelasticity.disc_neo_hookean
+
+    # Defines a function to test the Cauchy-continuum HGO model
+    
+    def test_cauchyHGO(self):
+
+        print("\n#####################################################"+
+        "###################\n#                              Cauchy HG"+
+        "O                              #\n###########################"+
+        "#############################################\n")
+
+        import tests.hyperelasticity.disc_HGO
+
+    # Defines a function to test the Micropolar-continuum Neo-Hookean 
+    # model
+    
+    def test_micropolarNeoHookean(self):
+
+        print("\n#####################################################"+
+        "###################\n#                        Micropolar Neo-"+
+        "Hookean                        #\n###########################"+
+        "#############################################\n")
+
+        import tests.micropolar.our_beam_1.beam_with_fibers_macroscale
+
+    # Defines a function to test the Micropolar-continuum Neo-Hookean 
+    # model microscale
+    
+    def test_micropolarNeoHookeanMicroscale(self):
+
+        print("\n#####################################################"+
+        "###################\n#                   Micropolar Neo-Hooke"+
+        "an Microscale                  #\n###########################"+
+        "#############################################\n")
+
+        import tests.micropolar.our_beam_1.beam_with_fibers_microscale
+
+# Runs all tests
+
+if __name__ == "__main__":
+
+    unittest.main()
